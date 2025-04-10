d9eb96bced7fa014c60d7374d12b444d76ce73bb
albel260819 <albel260819@gmail.com>
2025-04-10 22:15:13 +0600

    поправил функци недоделано

--- kalkulator.py
+++ kalkulator.py
@@ -2,35 +2,100 @@ from tkinter import *
 from tkinter import ttk
 window = Tk()
 window.title("калькулятор")
-window.geometry("327x225")
+#window.geometry("327x225")
 
 e1 = ttk.Entry(window)
 
-def sd():
-    num1 = float(btn1.get())
-    num2 = float(btn2.get())
-    result = num1 + num2
-    ttk.Label(window, text=result,
-                background="#FF8C00", foreground="#FFFFFF").place(relx=0, height=59, width=245)
-        
+print(eval("153+561*4"))
+
+
+def out(a):
+    print("123123123")
+
+string = ''
+
+def command_1():
+    global string
+    string += "1"
+def command_2():
+    global string
+    string += "2"
+def command_3():
+    global string
+    string += "3"
+def command_multiplication():
+    global string
+    string += "*"
+
+def command_4():
+    global string
+    string += "4"
+def command_5():
+    global string
+    string += "5"
+def command_6():
+    global string
+    string += "6"
+def command_division():
+    global string
+    string += "/"
+
+def command_7():
+    global string
+    string += "7"
+def command_8():
+    global string
+    string += "8"
+def command_9():
+    global string
+    string += "9"
+def command_equals():
+    global string
+    #string += "="
+    print(eval(string))
+
+def command_plus():
+    global string
+    string += "+"
+def command_minus():
+    global string
+    string += "-"
+def command_percent():
+    global string
+    string += "%"
+def command_zero():
+    global string
+    string += "0"
 
 ttk.Label(window, text="калькулятор\nдля\nмужиков", background="black",
            foreground="red").grid(ipady=5, column=3, row=0, ipadx=4)
-btn1 = ttk.Button(window, text="1", command=sd()).grid(column=0, row=1, ipadx=3, ipady=8)
-btn2 = ttk.Button(window, text="2", command=sd()).grid(column=1, row=1, ipadx=3, ipady=8)            
-ttk.Button(window, text="3", command=None).grid(column=2, row=1, ipadx=3, ipady=8)            
-ttk.Button(window, text="4", command=None).grid(column=0, row=2, ipadx=3, ipady=8)
-ttk.Button(window, text="5", command=None).grid(column=1, row=2, ipadx=3, ipady=8)
-ttk.Button(window, text="6", command=None).grid(column=2, row=2, ipadx=3, ipady=8)
-ttk.Button(window, text="7", command=None).grid(column=0, row=3, ipadx=3, ipady=8)
-ttk.Button(window, text="8", command=None).grid(column=1, row=3, ipadx=3, ipady=8)
-ttk.Button(window, text="9", command=None).grid(column=2, row=3, ipadx=3, ipady=8)
-ttk.Button(window, text="+", command=None).grid(column=0, row=4, ipadx=3, ipady=9)
-ttk.Button(window, text="-", command=None).grid(column=1, row=4, ipadx=3, ipady=9)
-ttk.Button(window, text="*", command=None).grid(column=3, row=1, ipadx=3, ipady=8)
-ttk.Button(window, text="/", command=None).grid(column=3, row=2, ipadx=3, ipady=8)
-ttk.Button(window, text="%", command=None).grid(column=2, row=4, ipadx=3, ipady=9)
-ttk.Button(window, text="=", command=None).grid(column=3, row=3, rowspan=2, ipady=30, ipadx=3)
+           
+buttons = [
+ttk.Button(window, text="1", command=command_1),
+ttk.Button(window, text="2", command=command_2),
+ttk.Button(window, text="3", command=command_3),
+ttk.Button(window, text="*", command=None),
+
+ttk.Button(window, text="4", command=None),
+ttk.Button(window, text="5", command=None),
+ttk.Button(window, text="6", command=None),
+ttk.Button(window, text="/", command=None),
+
+ttk.Button(window, text="7", command=None),
+ttk.Button(window, text="8", command=None),
+ttk.Button(window, text="9", command=None),
+ttk.Button(window, text="=", command=command_equals),
+
+ttk.Button(window, text="+", command=command_plus),
+ttk.Button(window, text="-", command=None),
+ttk.Button(window, text="%", command=None),
+ttk.Button(window, text="0", command=None)]
+
+for i in range(0, 4):
+    for j in range(0, 4):
+        buttons[i*4+j].grid(column=j, row=i+1, ipadx=3, ipady=8)
+
+
 
 
 
