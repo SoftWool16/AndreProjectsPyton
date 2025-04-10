3643e80f61e84042001b8c2bb63ab6edb5244e49
andre1315 <korabelskijandrej2010@gmail.com>
2025-04-10 21:30:57 +0300

    агагагга

--- kalkulator.py
+++ kalkulator.py
@@ -6,10 +6,18 @@ window.geometry("327x225")
 
 e1 = ttk.Entry(window)
 
+def sd():
+    num1 = float(btn1.get())
+    num2 = float(btn2.get())
+    result = num1 + num2
+    ttk.Label(window, text=result,
+                background="#FF8C00", foreground="#FFFFFF").place(relx=0, height=59, width=245)
+        
+
 ttk.Label(window, text="калькулятор\nдля\nмужиков", background="black",
            foreground="red").grid(ipady=5, column=3, row=0, ipadx=4)
-ttk.Button(window, text="1", command=None).grid(column=0, row=1, ipadx=3, ipady=8)
-ttk.Button(window, text="2", command=None).grid(column=1, row=1, ipadx=3, ipady=8)            
+btn1 = ttk.Button(window, text="1", command=sd()).grid(column=0, row=1, ipadx=3, ipady=8)
+btn2 = ttk.Button(window, text="2", command=sd()).grid(column=1, row=1, ipadx=3, ipady=8)            
 ttk.Button(window, text="3", command=None).grid(column=2, row=1, ipadx=3, ipady=8)            
 ttk.Button(window, text="4", command=None).grid(column=0, row=2, ipadx=3, ipady=8)
 ttk.Button(window, text="5", command=None).grid(column=1, row=2, ipadx=3, ipady=8)
@@ -26,8 +34,9 @@ ttk.Button(window, text="=", command=None).grid(column=3, row=3, rowspan=2, ipad
 
 
 
-ttk.Label(window, text="",
-        background="#FF8C00", foreground="#FFFFFF").place(relx=0, height=59, width=245)
+
+
+
 
 
 
