62d73d1302b56b406e2809b3dfa23dd29873bb95
andre1315 <korabelskijandrej2010@gmail.com>
2025-04-26 20:10:26 +0300

    пытаюсь сделать сортировку пузырьком

--- sort_1.py
+++ sort_1.py
@@ -1,42 +1,25 @@
-n = int(input())
-import random                                            #\
-lists = ""      #создание пустого списка                  #\                
-a = list(range(n)) #ввод размера массива                   #\
-random.shuffle(a)   #перемешивание массива               |создание и перемешивание массива
-for b in a:                                                #/
-    lists += (" | " + str(b))                             #/                                                                        
-print(lists)                                             #/
+num = int(input())
+import random
+string = ""      #создание пустого списка
+a = list(range(num)) #ввод размера массива
+random.shuffle(a)   #перемешивание массива
+for b in a:
+    string += (" | " + str(b))
+print(string)
 
 
-print(lists.split(" | "))
+def ids(n1, n2):
+    if n1 > n2:
+        s = a.pop(n1)
+        a.insert(n2, s)
+        print(a)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+for number in a:
+    index = a.index(number)
+    for number_2 in a:
+        index_2 = a.index(number_2)
+        ids(index, index_2)
 
 
 
